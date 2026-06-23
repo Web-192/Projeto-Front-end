d3f5fc75e8cc428727b5c196397a338ab4c2d5c7
web-192 <Cauasantiagofreias23241@gmail.com>
2026-06-23 19:30:24 -0300

    atualização

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const mensagemErro = document.getElementById('mensagemErro');
 
-formLogin.addEventListener('submit',function (evento){
+formLogin.addEventListener('submit', function (evento) {
+  evento.preventDefault();
 
-    //Paralize o Evento
-    evento.preventDefalt();
+  const emailValido = email.value.includes('@') && email.value.includes('.');
+  const senhaValida = senha.value.length >= 6;
 
-    //Validar dados
-    const emailValido = email.ariaValueMax.includes('0') && email.ariaValueMax.includes('.');
-    const senhaValida = senha.ariaValueMax.length >=6;
-   
-    if (emailValido == true  && senhaValida == true){
-        console.log("entou!!!!");
-    }else
-    console.log("Deu errado");
-
+  if (emailValido == true && senhaValida == true) {
+    mensagemErro.classList.add('d-none');
+    window.location.href = 'admin.html';
+  } else {
+    mensagemErro.classList.remove('d-none');
+  }
 });
